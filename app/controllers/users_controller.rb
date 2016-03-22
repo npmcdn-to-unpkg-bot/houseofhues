60ccb46b7d882b4be5019f2f4e9bdffe6dc6d39a
Alison Bonham <alison@alisonmae.com>
2016-03-22 14:15:51 -0600

    user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_filter :authenticate_user!
   def new
   end
 
@@ -6,5 +7,18 @@ class UsersController < ApplicationController
   end
 
   def show
+  @user = User.find(params[:id])
+  @user_email = @user.email
+  @last_login = @user.last_sign_in_at
+  @avatar = @user.avatar
+  #submitted looks
+  #boosts recieved
+  #boosts given
   end
+
+  def user_collection
+    @user = User.find(params[:id])
+    @user_email = @user.email
+  end
+
 end
