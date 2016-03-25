dcefe9e3864bec569a7d649e28c2861ee3ea51eb
Alison Bonham <alison@alisonmae.com>
2016-03-25 13:01:15 -0600

    orchid from orchard seeds

--- db/schema.rb
+++ db/schema.rb
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 20160324171911) do
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
+    t.string   "avatar"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -70,11 +71,6 @@ ActiveRecord::Schema.define(version: 20160324171911) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "avatar"
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
