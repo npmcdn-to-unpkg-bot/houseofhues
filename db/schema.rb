59afe3b705aa07058fad20d5009f8c28f75e5f50
James Sanders <mrjamesrsanders@gmail.com>
2016-03-25 11:38:44 -0500

    devise form styling

--- db/schema.rb
+++ db/schema.rb
@@ -60,7 +60,6 @@ ActiveRecord::Schema.define(version: 20160324171911) do
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
-    t.string   "avatar"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -76,7 +75,6 @@ ActiveRecord::Schema.define(version: 20160324171911) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
-
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
